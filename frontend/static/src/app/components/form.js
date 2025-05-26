a6941adfcb4b94b444dea53160bed22f418f8bdc
Rexesezka <118622073+Rexesezka@users.noreply.github.com>
2025-05-26 23:24:08 +0500

    Update form.js

--- frontend/static/src/app/components/form.js
+++ frontend/static/src/app/components/form.js
@@ -33,7 +33,7 @@ const Form = ({isChecked}) => {
             post
         };
         try {
-            const response = await fetch('http://localhost:8000/api/submit-form/', {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/submit-form/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
